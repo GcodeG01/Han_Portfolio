2807706f13c483bbaa1100912c4d5c546f3e1b1b
Joseph Han <josephhan718@gmail.com>
2019-10-13 22:36:32 -0700

    'deploy'

---
+++ precache-manifest.e51915aff40c2fa262200b7629a81260.js
@@ -0,0 +1,406 @@
+self.__precacheManifest = [
+  {
+    "revision": "203215ebe53a31eb2e45b46d162a6e00",
+    "url": "/Han_Portfolio/img/right-arrow.203215eb.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Han_Portfolio/robots.txt"
+  },
+  {
+    "revision": "17025dee685dbf2cabb450e307391731",
+    "url": "/Han_Portfolio/img/small-22.17025dee.png"
+  },
+  {
+    "revision": "a284243652fb16d3db57",
+    "url": "/Han_Portfolio/js/chunk-vendors.3187719c.js"
+  },
+  {
+    "revision": "88200b5707b385ed3b628bdc18b077c6",
+    "url": "/Han_Portfolio/img/small-2.88200b57.png"
+  },
+  {
+    "revision": "44339da7dd3737f05e5f2a8d2c64897c",
+    "url": "/Han_Portfolio/img/small-8.44339da7.png"
+  },
+  {
+    "revision": "a96748fa89fe22158a9b37cae8f66c2d",
+    "url": "/Han_Portfolio/index.html"
+  },
+  {
+    "revision": "16fb551098373a73b50d1262c1d3c061",
+    "url": "/Han_Portfolio/img/store-1.16fb5510.png"
+  },
+  {
+    "revision": "ae48c6bdb1b0a8f57b76f9789a35c6f8",
+    "url": "/Han_Portfolio/img/work.ae48c6bd.png"
+  },
+  {
+    "revision": "f49db64b55457c186c1a198c505130cd",
+    "url": "/Han_Portfolio/img/small-9.f49db64b.png"
+  },
+  {
+    "revision": "2648cde6bc42919cabb6ef7a28e4b0d2",
+    "url": "/Han_Portfolio/img/store-2.2648cde6.png"
+  },
+  {
+    "revision": "c643055694bd1e50f726",
+    "url": "/Han_Portfolio/js/app.7edef925.js"
+  },
+  {
+    "revision": "f723a74a51a8f0d086fefe27064144e1",
+    "url": "/Han_Portfolio/img/stapler.f723a74a.png"
+  },
+  {
+    "revision": "2641a042ea7c4c6e0889199e9533409b",
+    "url": "/Han_Portfolio/img/toast.2641a042.gif"
+  },
+  {
+    "revision": "175ebb85f88cbc067188102ef7c9c5d7",
+    "url": "/Han_Portfolio/img/workSVG.175ebb85.svg"
+  },
+  {
+    "revision": "dafecc251ca526d188fc3aff3c692baa",
+    "url": "/Han_Portfolio/img/art-10.dafecc25.png"
+  },
+  {
+    "revision": "9e4cf3d13395d11e71fc5363bc1e6b2b",
+    "url": "/Han_Portfolio/img/large-21.9e4cf3d1.png"
+  },
+  {
+    "revision": "e8ebae3ae14273a7244ce2e0e23b85d6",
+    "url": "/Han_Portfolio/img/small-12.e8ebae3a.png"
+  },
+  {
+    "revision": "405f96e9a1536c88277e4743a27b815c",
+    "url": "/Han_Portfolio/img/large-29.405f96e9.png"
+  },
+  {
+    "revision": "a90389a5b9b135385158a9738bbd6e89",
+    "url": "/Han_Portfolio/img/small-26.a90389a5.png"
+  },
+  {
+    "revision": "3ff1a9438658707ddb69d904e30b271d",
+    "url": "/Han_Portfolio/img/small-13.3ff1a943.png"
+  },
+  {
+    "revision": "1eae616d4a3f4bcf319e08e72152c6bc",
+    "url": "/Han_Portfolio/img/large-25.1eae616d.png"
+  },
+  {
+    "revision": "34c296cd80744c3be5ba15cf966d61c1",
+    "url": "/Han_Portfolio/img/ramp.34c296cd.png"
+  },
+  {
+    "revision": "ad3cf466971b82d33e19035c54c9b4af",
+    "url": "/Han_Portfolio/img/skillSVG.ad3cf466.svg"
+  },
+  {
+    "revision": "c819e4a1902d237d13a69681ba56b3ab",
+    "url": "/Han_Portfolio/img/large-4.c819e4a1.png"
+  },
+  {
+    "revision": "7ffcb0922f657c0159eef7c2e658aefc",
+    "url": "/Han_Portfolio/img/left-arrow.7ffcb092.png"
+  },
+  {
+    "revision": "859368df0633b4f4aa887e30d136322d",
+    "url": "/Han_Portfolio/img/large-6.859368df.png"
+  },
+  {
+    "revision": "59a9d6b5c2ea3004b6a3351c4f22a341",
+    "url": "/Han_Portfolio/img/large-22.59a9d6b5.png"
+  },
+  {
+    "revision": "ca99f1bc90905bd5f661d918defc65b7",
+    "url": "/Han_Portfolio/img/small-5.ca99f1bc.png"
+  },
+  {
+    "revision": "f34c592b34676cad5511214c14737a84",
+    "url": "/Han_Portfolio/img/small-23.f34c592b.png"
+  },
+  {
+    "revision": "f9532331b426574fe8bd9e60ca03a9aa",
+    "url": "/Han_Portfolio/img/small-11.f9532331.png"
+  },
+  {
+    "revision": "bf9479c961ba7df8e0f717d8782b570b",
+    "url": "/Han_Portfolio/img/large-7.bf9479c9.png"
+  },
+  {
+    "revision": "f3076dd329af72e3130e215c8871d3ea",
+    "url": "/Han_Portfolio/img/portfolio.f3076dd3.png"
+  },
+  {
+    "revision": "f753617a4deab26a59a83bfd05b5e1bb",
+    "url": "/Han_Portfolio/img/large-3.f753617a.png"
+  },
+  {
+    "revision": "4521485b2ae524a4a54539348d837a70",
+    "url": "/Han_Portfolio/img/large-5.4521485b.png"
+  },
+  {
+    "revision": "7355ce743fb7506a024145bcb55fff4a",
+    "url": "/Han_Portfolio/img/small-14.7355ce74.png"
+  },
+  {
+    "revision": "8bce864464fd983b0c3854b459b6b891",
+    "url": "/Han_Portfolio/img/small-24.8bce8644.png"
+  },
+  {
+    "revision": "c853abc3bd91b77a192e7142c17e6ff7",
+    "url": "/Han_Portfolio/img/small-21.c853abc3.png"
+  },
+  {
+    "revision": "ca88da954474d5071e74020674c00620",
+    "url": "/Han_Portfolio/img/small-16.ca88da95.png"
+  },
+  {
+    "revision": "5b6cac3584bf51fbc286a29cd5acce02",
+    "url": "/Han_Portfolio/img/large-24.5b6cac35.png"
+  },
+  {
+    "revision": "db9c74f6e85e7cfb0dd19a570fea22de",
+    "url": "/Han_Portfolio/img/small-4.db9c74f6.png"
+  },
+  {
+    "revision": "8dddbed05a3c47833fac649a6f8320e5",
+    "url": "/Han_Portfolio/img/small-3.8dddbed0.png"
+  },
+  {
+    "revision": "f7c5ef0cc3914ea9a8a783652f1ae323",
+    "url": "/Han_Portfolio/img/small-10.f7c5ef0c.png"
+  },
+  {
+    "revision": "93ebf700451be136a40d97bfceb3cc4f",
+    "url": "/Han_Portfolio/img/omnifood.93ebf700.png"
+  },
+  {
+    "revision": "72dbcb6e6b5df40cd5906f8295293d26",
+    "url": "/Han_Portfolio/img/large-23.72dbcb6e.png"
+  },
+  {
+    "revision": "1df657d609098b00da2c34ff86cfe17f",
+    "url": "/Han_Portfolio/img/small-6.1df657d6.png"
+  },
+  {
+    "revision": "8bb659af7e749788147d831d4072dc44",
+    "url": "/Han_Portfolio/img/small-29.8bb659af.png"
+  },
+  {
+    "revision": "8438a3ee5fd66628d8061401d5486485",
+    "url": "/Han_Portfolio/img/small-18.8438a3ee.png"
+  },
+  {
+    "revision": "f86ac739018a4c6427f91841605ce05b",
+    "url": "/Han_Portfolio/img/small-17.f86ac739.png"
+  },
+  {
+    "revision": "9c3beb87cfe3e531b955a04057391179",
+    "url": "/Han_Portfolio/img/small-27.9c3beb87.png"
+  },
+  {
+    "revision": "9fc6046c5ed19b88d1f231cb68caa420",
+    "url": "/Han_Portfolio/img/large-8.9fc6046c.png"
+  },
+  {
+    "revision": "12d071678a1b4a1d0155265ceeb0bf1e",
+    "url": "/Han_Portfolio/img/skill.12d07167.png"
+  },
+  {
+    "revision": "505363776c569ea896358b156eede904",
+    "url": "/Han_Portfolio/img/small-20.50536377.png"
+  },
+  {
+    "revision": "8839915d72b22a9cdf680bf6ae93bbc5",
+    "url": "/Han_Portfolio/img/small-1.8839915d.png"
+  },
+  {
+    "revision": "53c5bab9d76c23cbeeedc6a0d7bd2d7a",
+    "url": "/Han_Portfolio/img/natours.53c5bab9.png"
+  },
+  {
+    "revision": "5cb68a1d328bedfb1853232a05559ed7",
+    "url": "/Han_Portfolio/img/small-25.5cb68a1d.png"
+  },
+  {
+    "revision": "d976965ca69ce5c72a2f06782c26a555",
+    "url": "/Han_Portfolio/img/small-28.d976965c.png"
+  },
+  {
+    "revision": "044a8108322b25b7d264402d4b81b929",
+    "url": "/Han_Portfolio/img/small-15.044a8108.png"
+  },
+  {
+    "revision": "7b387a434b5a47494b1a95bff07f05e5",
+    "url": "/Han_Portfolio/img/large-9.7b387a43.png"
+  },
+  {
+    "revision": "d50a42c3470a753da370b86cb3932956",
+    "url": "/Han_Portfolio/img/small-19.d50a42c3.png"
+  },
+  {
+    "revision": "b3b0bfedd5adb9aed09c876d31bf3b78",
+    "url": "/Han_Portfolio/img/shelf.b3b0bfed.png"
+  },
+  {
+    "revision": "be66c18722e2977886d5b3a00a569064",
+    "url": "/Han_Portfolio/img/large-26.be66c187.png"
+  },
+  {
+    "revision": "ee83d412fd9c482b961346935ccb5393",
+    "url": "/Han_Portfolio/img/large-27.ee83d412.png"
+  },
+  {
+    "revision": "a717475f1111bc866f2fa08f69ebe61b",
+    "url": "/Han_Portfolio/img/small-7.a717475f.png"
+  },
+  {
+    "revision": "1027f79c3139ccc010b48f9a893f8189",
+    "url": "/Han_Portfolio/img/large-28.1027f79c.png"
+  },
+  {
+    "revision": "ed480a5069b264784cbd3e9ce0393ef4",
+    "url": "/Han_Portfolio/img/pen.ed480a50.png"
+  },
+  {
+    "revision": "13e1da14c31633ddf72f0fa01ee06f07",
+    "url": "/Han_Portfolio/img/large-14.13e1da14.png"
+  },
+  {
+    "revision": "e1c2058dbcb27271ef1c79285f256e4d",
+    "url": "/Han_Portfolio/img/large-15.e1c2058d.png"
+  },
+  {
+    "revision": "cd44cccbbd8be9f0f2a5f00f3fbf9e64",
+    "url": "/Han_Portfolio/img/large-18.cd44cccb.png"
+  },
+  {
+    "revision": "5b48e6413086023a91975a08440f23df",
+    "url": "/Han_Portfolio/img/large-20.5b48e641.png"
+  },
+  {
+    "revision": "c31e385a50def31b9450f5facf9d69a9",
+    "url": "/Han_Portfolio/img/large-19.c31e385a.png"
+  },
+  {
+    "revision": "e9094342ab161bb0d601a174045fcec1",
+    "url": "/Han_Portfolio/img/large-2.e9094342.png"
+  },
+  {
+    "revision": "dbce9c6fb9182c15e4696add5f4038d9",
+    "url": "/Han_Portfolio/img/large-16.dbce9c6f.png"
+  },
+  {
+    "revision": "140cf147ce1b54983a3b26668185fc94",
+    "url": "/Han_Portfolio/img/large-17.140cf147.png"
+  },
+  {
+    "revision": "c1a507d5d3e7bcc2fd95558d57626e51",
+    "url": "/Han_Portfolio/img/large-10.c1a507d5.png"
+  },
+  {
+    "revision": "334fff03e7eb7807183865aa882e8cf0",
+    "url": "/Han_Portfolio/img/art-8.334fff03.png"
+  },
+  {
+    "revision": "a29dc3ba79920570e8375dd47256bf91",
+    "url": "/Han_Portfolio/img/art-9.a29dc3ba.png"
+  },
+  {
+    "revision": "a328facf6dfaea3790da54e22afa2bf1",
+    "url": "/Han_Portfolio/img/boat.a328facf.png"
+  },
+  {
+    "revision": "ca7de7d45f777f632c097ef54ffd716c",
+    "url": "/Han_Portfolio/img/d20.ca7de7d4.png"
+  },
+  {
+    "revision": "e475b7570ed6b78389a68007b02a9464",
+    "url": "/Han_Portfolio/img/desk.e475b757.png"
+  },
+  {
+    "revision": "b18103a42c5804f46ecef3beda3e8ce6",
+    "url": "/Han_Portfolio/img/info.b18103a4.png"
+  },
+  {
+    "revision": "c4f99f043a31b1aad55ca5945451f902",
+    "url": "/Han_Portfolio/img/large-13.c4f99f04.png"
+  },
+  {
+    "revision": "30ac45de7533a5084aa6ce552ac600cd",
+    "url": "/Han_Portfolio/img/large-1.30ac45de.png"
+  },
+  {
+    "revision": "8660b701e7f3962d68e6c7fba7ea025a",
+    "url": "/Han_Portfolio/img/hand.8660b701.png"
+  },
+  {
+    "revision": "1f722d3e2ea2c86f20126f4af4f4a212",
+    "url": "/Han_Portfolio/img/large-12.1f722d3e.png"
+  },
+  {
+    "revision": "55652cc00612737a6cead92866b5cd1b",
+    "url": "/Han_Portfolio/img/fishing.55652cc0.gif"
+  },
+  {
+    "revision": "13842ede495ed407f287de10f4337d4f",
+    "url": "/Han_Portfolio/img/batmobile.13842ede.png"
+  },
+  {
+    "revision": "c1eeb83d0fe2822e0171b429ae539715",
+    "url": "/Han_Portfolio/img/building.c1eeb83d.png"
+  },
+  {
+    "revision": "895fd23b1fef775c6233ed290b5e95df",
+    "url": "/Han_Portfolio/img/infoSVG.895fd23b.svg"
+  },
+  {
+    "revision": "9d4306a7ab4f64828ae30c48943a1a55",
+    "url": "/Han_Portfolio/img/large-11.9d4306a7.png"
+  },
+  {
+    "revision": "7a1ed77a8d7b3e9778e4ca200bbf82aa",
+    "url": "/Han_Portfolio/img/clock.7a1ed77a.png"
+  },
+  {
+    "revision": "489ffe0c9fc2f3d6af34fd4ceff01d74",
+    "url": "/Han_Portfolio/img/art-7.489ffe0c.png"
+  },
+  {
+    "revision": "90ee12ed04eee765b8b7bdff186b502b",
+    "url": "/Han_Portfolio/img/art-6.90ee12ed.png"
+  },
+  {
+    "revision": "282f13559168e47dcfcbe7173da0a7ce",
+    "url": "/Han_Portfolio/img/art-5.282f1355.png"
+  },
+  {
+    "revision": "5aeb479064a4126f657379803f87a978",
+    "url": "/Han_Portfolio/img/art-4.5aeb4790.png"
+  },
+  {
+    "revision": "d3e5f5a6ea8ee2f28e25c9921982fe13",
+    "url": "/Han_Portfolio/img/art-3.d3e5f5a6.png"
+  },
+  {
+    "revision": "7eb087e3d8f37e1482bd4056cc1b8741",
+    "url": "/Han_Portfolio/img/art-2.7eb087e3.png"
+  },
+  {
+    "revision": "be410fbf7f96d7b3160ccc9f6dcaef2f",
+    "url": "/Han_Portfolio/img/art-1.be410fbf.png"
+  },
+  {
+    "revision": "dc775d907980d74193a6995b5ef6c878",
+    "url": "/Han_Portfolio/img/apex.dc775d90.png"
+  },
+  {
+    "revision": "3e3e2510c6b3d240aec40ddf8946fb63",
+    "url": "/Han_Portfolio/img/acs.3e3e2510.png"
+  },
+  {
+    "revision": "c643055694bd1e50f726",
+    "url": "/Han_Portfolio/css/app.6b206700.css"
+  }
+];
